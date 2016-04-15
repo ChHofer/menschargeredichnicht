d96a220f38a961841d1f8ba23c65fac3923c513d
Oliver <omauerhofer@gmail.com>
2016-04-15 21:09:44 +0200

    Menü angelegt. Spielbrett Dummy angelegt um Menüführung zu testen. Menü als "Home-Activity" festgelegt

---
+++ app/src/main/java/at/se2/gruppe3/menschrgeredichnicht/MenuActivity.java
@@ -0,0 +1,110 @@
+package at.se2.gruppe3.menschrgeredichnicht;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by oliver on 15.04.16.
+ */
+public class MenuActivity extends Activity implements View.OnClickListener {
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    Button btnNewGame, btnJoinGame, btnHelp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        initialize();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Menu Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://at.se2.gruppe3.menschrgeredichnicht/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Menu Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://at.se2.gruppe3.menschrgeredichnicht/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.btnNewGame:
+                Intent newGameScreen = new Intent(getApplicationContext(),
+                        Spielbrett.class);
+                startActivity(newGameScreen);
+                finish();
+                break;
+            case R.id.btnJoinGame:
+                this.finish();
+                break;
+            case R.id.btnHelp:
+                this.finish();
+                break;
+
+        }
+    }
+
+    public void initialize(){
+        btnNewGame = (Button) findViewById(R.id.btnNewGame);
+        btnNewGame.setOnClickListener(this);
+        btnJoinGame = (Button) findViewById(R.id.btnJoinGame);
+        btnJoinGame.setOnClickListener(this);
+        btnHelp = (Button) findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener(this);
+
+    }
+
+}
