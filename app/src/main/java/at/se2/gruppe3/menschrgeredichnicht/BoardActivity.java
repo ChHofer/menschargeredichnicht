ac61fd663964deac974c40b01b6d9912b61bbe8d
ChHofer <chofer@edu.aau.at>
2016-05-02 10:12:56 +0200

    Spielbrett: Kegel Bewegen und Fix für Darstellung

--- app/src/main/java/at/se2/gruppe3/menschrgeredichnicht/BoardActivity.java
+++ app/src/main/java/at/se2/gruppe3/menschrgeredichnicht/BoardActivity.java
@@ -2,19 +2,20 @@ package at.se2.gruppe3.menschrgeredichnicht;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
+import android.util.Log;
 import android.widget.RelativeLayout;
 
-public class BoardActivity extends Activity {
+public class BoardActivity extends Activity implements BoardView.OnFeldClickedListener{
 
-    private BoardView BoardView;
+    private BoardView BView;
     private RelativeLayout TableView;
 
     private Kegel[][] StartFelder;
     private Kegel[][] ZielFelder;
 
     private Kegel[] HauptFelder;
+
+    Kegel KegelHighlighted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +30,99 @@ public class BoardActivity extends Activity {
         TableView = (RelativeLayout)findViewById(R.id.tableView);
         TableView.setBackgroundResource(R.drawable.table);
 
-        BoardView = (BoardView)findViewById(R.id.boardView);
-        BoardView.setImageResource(R.drawable.board);
+        BView = (BoardView)findViewById(R.id.boardView);
+        BView.setImageResource(R.drawable.board);
 
-        BoardView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                    moveDone();
-                return true;
+        startGame();
+        moveDone();
+
+    }
+
+    public void startGame(){
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=3;j++){
+                StartFelder[i][j] = new Kegel(i,0,j);
             }
-        });
+        }
+        Log.w("Logger","StartGame");
     }
 
     public void moveDone(){
-        BoardView.invalidate();
+        BView.setBoardState(StartFelder,ZielFelder,HauptFelder);
+        BView.invalidate();
+        Log.w("Logger","MoveDone");
     }
 
+    @Override
+    public void OnFeldClicked(int state,int player,int position) {
+        switch(state){
+            case 0:
+                if(StartFelder[player][position]!=null){
+                    KegelHighlighted = StartFelder[player][position];
+                    BView.highlightKegel(KegelHighlighted);
+                }
+                break;
+            case 1:
+                if(KegelHighlighted!=null){
+                    moveKegel(KegelHighlighted,state,position);
+                }else{
+                    KegelHighlighted = HauptFelder[position];
+                    BView.highlightKegel(KegelHighlighted);
+                }
+                break;
+            case 2:
+                if(KegelHighlighted!=null){
+                    moveKegel(KegelHighlighted,state,position);
+                }else{
+                    KegelHighlighted = ZielFelder[player][position];
+                    BView.highlightKegel(KegelHighlighted);
+                }
+                break;
+        }
+        moveDone();
+    }
+
+    public void moveKegel(Kegel k,int state, int position){
+        removeKegel(k);
+        switch(state) {
+            case 0:
+                StartFelder[k.getPlayer()][position] = new Kegel(k.getPlayer(),state,position);
+                break;
+            case 1:
+                if(HauptFelder[position]!=null){
+                    kickKegel(HauptFelder[position]);
+                }
+                HauptFelder[position] = new Kegel(k.getPlayer(),state,position);
+                break;
+            case 2:
+                ZielFelder[k.getPlayer()][position] = new Kegel(k.getPlayer(),state,position);
+                break;
+        }
+        BView.resetHighlight();
+        KegelHighlighted = null;
+        moveDone();
+    }
+
+    public void kickKegel(Kegel k){
+        for(int i=0;i<StartFelder[k.getPlayer()].length;i++){
+            if(StartFelder[k.getPlayer()][i]==null){
+                StartFelder[k.getPlayer()][i] = new Kegel(k.getPlayer(),0,i);
+                break;
+            }
+        }
+    }
+
+    public void removeKegel(Kegel k){
+        switch(k.getState()) {
+            case 0:
+                StartFelder[k.getPlayer()][k.getPosition()] = null;
+                break;
+            case 1:
+                HauptFelder[k.getPosition()] = null;
+                break;
+            case 2:
+                ZielFelder[k.getPlayer()][k.getPosition()] = null;
+                break;
+        }
+    }
 }
